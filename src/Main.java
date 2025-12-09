1c7bd77fef17f1524ef8743497a9c849610da660
saptadipa77 <saptadipadatta5@gmail.com>
2025-12-09 19:57:59 +0530

    initial push

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Single Player Snake & Ladder Game");
+        Player p1 = new Player();
+        System.out.println("current position of player"+" "+p1.getPosition());
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,7 @@
+public class Player {
+    private int position = 0;
+
+    public int getPosition() {
+        return position;
+    }
+}
